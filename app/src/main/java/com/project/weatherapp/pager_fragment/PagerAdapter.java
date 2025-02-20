fb04a6abd186b9c9bb9a57294583630d55e1687b
junho <junho5508@naver.com>
2025-02-20 23:24:22 +0900

    createFragment 메서드의 case1의 Future()에 dataModel추가

--- app/src/main/java/com/project/weatherapp/pager_fragment/PagerAdapter.java
+++ app/src/main/java/com/project/weatherapp/pager_fragment/PagerAdapter.java
@@ -7,18 +7,22 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import com.project.weatherapp.futureRecyclerView.RecyclerDataModel;
+
 public class PagerAdapter extends FragmentStateAdapter {
     private String temperature, precipitation, humidity, precipitationType, skyCondition;
+    private RecyclerDataModel dataModel;
 
     public PagerAdapter(@NonNull FragmentActivity fragmentActivity,
                         String temperature, String precipitation, String humidity,
-                        String precipitationType, String skyCondition) {
+                        String precipitationType, String skyCondition, RecyclerDataModel dataModel) {
         super(fragmentActivity);
         this.temperature = temperature;
         this.precipitation = precipitation;
         this.humidity = humidity;
         this.precipitationType = precipitationType;
         this.skyCondition = skyCondition;
+        this.dataModel = dataModel;
         Log.d("ShowWeather", "setViewPager() 호출됨");
         Log.d("data test", "temperature:" + temperature + "\nprecipitation:" + precipitation);
 
@@ -32,7 +36,7 @@ public class PagerAdapter extends FragmentStateAdapter {
                 return new Current(temperature, precipitation, humidity, precipitationType,
                         skyCondition);
             case 1:
-                return new Future();
+                return new Future(dataModel);
             default:
                 return new Current(temperature, precipitation, humidity, precipitationType,
                         skyCondition);
